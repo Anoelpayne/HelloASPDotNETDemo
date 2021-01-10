c267ae5a8c1c406155b566d491e9bce562613de9
Andrea Payne <anoelpayne@gmail.com>
2021-01-10 12:34:35 -0600

    First step

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -6,22 +6,17 @@ using System.Threading.Tasks;
 
 namespace HelloASPDotNET.Controllers
 {
-    [Route("/helloworld/")]
+    [Route("/hello/")]
     public class HelloController : Controller
     {   // GET: /<controller>/
         [HttpGet]
         //[Route("/helloworld/")]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloworld/'>" +
-                       "<input type='text' name='name' />" +
-                       "<input type='submit' value='Greet Me!' />" +
-                       "</form>";
-
-            return Content(html, "text/html");
+            return View();
         }
         // GET /hello/welcome
-        [HttpGet("welcome/{name?}")]
+
         [HttpPost]
         // [Route("/helloworld/")]
         public IActionResult Welcome(string name = "World")
